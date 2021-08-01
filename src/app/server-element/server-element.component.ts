d08cf2e77281bec38f19a2aa788c46232399d55f
Christopher II Lajom <christopheriilajom@gmail.com>
2021-08-01 18:51:42 +0800

    Added some lifecycle hooks in server-element component.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,43 @@
-import { Component, Input, OnInit } from "@angular/core";
+import {
+  AfterContentInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from "@angular/core";
 
 @Component({
   selector: "app-server-element",
   templateUrl: "./server-element.component.html",
   styleUrls: ["./server-element.component.css"],
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements OnInit, OnChanges, DoCheck, AfterContentInit
+{
   @Input("element") element: Element;
+  @Input() name: string;
 
-  constructor() {}
+  constructor() {
+    console.log("constructor called");
+  }
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges called", changes);
+  }
+
+  ngOnInit(): void {
+    console.log("ngoninit called");
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck called");
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit called");
+  }
 }
 
 interface Element {
